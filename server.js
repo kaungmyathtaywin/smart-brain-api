03f9c5f92f38bd56c288dc2985d6d1c33123a1bb
kaungmyathtaywin <kaungmyathtaywin@gmail.com>
2021-04-03 15:23:58 +0630

    Join signin and register api with database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import knex from "knex";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -18,59 +19,66 @@ const db = knex({
 app.use(cors());
 app.use(express.json());
 
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "John Doe",
-      email: "john@gmail.com",
-      password: "cookies",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Doe",
-      email: "doe@gmail.com",
-      password: "monster",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
-
 app.get("/", (req, res) => {
-  res.json(database.users);
+  res.json("Hello World");
 });
 
 app.post("/signin", (req, res) => {
-  console.log(req.body);
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).send("Error logging in");
-  }
+  const { email, password } = req.body;
+  db.select("email", "hash")
+    .from("login")
+    .where({ email: email })
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where({ email: email })
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((error) => {
+            res.status(400).json("Could not find user");
+          });
+      } else {
+        res.status(400).json("Wrong credentials");
+      }
+    })
+    .catch((error) => {
+      res.status(400).json("Invaid credentials");
+    });
 });
 
 app.post("/register", (req, res) => {
-  const { name, email } = req.body;
+  const { name, email, password } = req.body;
+  const saltRounds = 10;
+  const hash = bcrypt.hashSync(password, saltRounds);
 
-  db("users")
-    .returning("*")
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((error) => {
-      res.status(400).json("Coult not register");
-    });
+  db.transaction((trx) => {
+    trx("login")
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .returning("email")
+      .then((loginEmail) => {
+        return db("users")
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .returning("*")
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((error) => {
+    res.status(400).json("Could not register");
+  });
 });
 
 app.get("/profile/:id", (req, res) => {
@@ -93,27 +101,18 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-
-  if (!found) {
-    res.status(400).send("No such user");
-  }
+  db("users")
+    .where({ id: id })
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((error) => {
+      res.status(400).json("Unable to get entries");
+    });
 });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-/*
-/signin => POST > success/fail
-/register => POST > user
-/profile/:id => GET > user
-/image => PUT > user
-*/
